edce90eba9bbf27a13e7046a3cfa1566068512a5
Nobi1202 <saobang001111@gmail.com>
2022-03-14 21:49:33 +0700

    update

--- lib/screens/home/components/new_arrival_products.dart
+++ lib/screens/home/components/new_arrival_products.dart
@@ -1,55 +1,55 @@
-// import 'package:flutter/material.dart';
-// import 'package:stylish/models/Product.dart';
-// import 'package:stylish/screens/details/details_screen.dart';
-//
-// import '../../../constants.dart';
-// import 'product_card.dart';
-// import 'section_title.dart';
-//
-// class NewArrivalProducts extends StatelessWidget {
-//   const NewArrivalProducts({
-//     Key? key,
-//   }) : super(key: key);
-//
-//   @override
-//   Widget build(BuildContext context) {
-//     return Column(
-//       children: [
-//         Padding(
-//           padding: const EdgeInsets.symmetric(vertical: defaultPadding),
-//           child: SectionTitle(
-//             title: "New Arrival",
-//             pressSeeAll: () {},
-//           ),
-//         ),
-//         SingleChildScrollView(
-//           physics: const BouncingScrollPhysics(
-//               parent: AlwaysScrollableScrollPhysics()),
-//           scrollDirection: Axis.horizontal,
-//           child: Row(
-//             children: List.generate(
-//               demo_product.length,
-//               (index) => Padding(
-//                 padding: const EdgeInsets.only(right: defaultPadding),
-//                 child: ProductCard(
-//                   title: demo_product[index].title,
-//                   image: demo_product[index].image,
-//                   price: demo_product[index].price,
-//                   bgColor: demo_product[index].bgColor,
-//                   press: () {
-//                     Navigator.push(
-//                         context,
-//                         MaterialPageRoute(
-//                           builder: (context) =>
-//                               DetailsScreen(product: demo_product[index]),
-//                         ));
-//                   },
-//                 ),
-//               ),
-//             ),
-//           ),
-//         )
-//       ],
-//     );
-//   }
-// }
+import 'package:flutter/material.dart';
+import 'package:stylish/models/Product.dart';
+import 'package:stylish/screens/details/details_screen.dart';
+
+import '../../../constants.dart';
+import 'product_card.dart';
+import 'section_title.dart';
+
+class NewArrivalProducts extends StatelessWidget {
+  const NewArrivalProducts({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        Padding(
+          padding: const EdgeInsets.symmetric(vertical: defaultPadding),
+          child: SectionTitle(
+            title: "New Arrival",
+            pressSeeAll: () {},
+          ),
+        ),
+        SingleChildScrollView(
+          physics: const BouncingScrollPhysics(
+              parent: AlwaysScrollableScrollPhysics()),
+          scrollDirection: Axis.horizontal,
+          child: Row(
+            children: List.generate(
+              demo_product.length,
+              (index) => Padding(
+                padding: const EdgeInsets.only(right: defaultPadding),
+                child: ProductCard(
+                  title: demo_product[index].title,
+                  image: demo_product[index].image,
+                  price: demo_product[index].price,
+                  bgColor: demo_product[index].bgColor,
+                  press: () {
+                    Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                          builder: (context) =>
+                              DetailsScreen(product: demo_product[index]),
+                        ));
+                  },
+                ),
+              ),
+            ),
+          ),
+        )
+      ],
+    );
+  }
+}
